bc3d567fc9cad85005df121fd6d9dafc49ac64fc
fehdi7 <128314465+fehdi7@users.noreply.github.com>
2023-05-06 00:56:41 +0530

    Add files via upload

---
+++ src/routes/main.js
@@ -0,0 +1,40 @@
+const express = require ("express")
+const { route } = require('express/lib/application')
+const Detail = require("../models/detail")
+const routes = express.Router()
+const path = require("path")
+const collection = require("../models/userinfo").default;
+
+routes.use(express.urlencoded({ extended: true }))
+
+routes.use(express.json())
+
+
+
+routes.get('/', (req,res) =>{
+	res.render("login")
+})
+ 
+routes.get('/signup', (req,res) =>{
+	res.render("signup")
+})
+
+
+routes.get('/home', async (req, res) => {
+	const Details= await Detail.findOne({"id":"64517f235a5fdfd776d5cca7"})
+	//console.log(Details)
+		res.render("index",{
+			Details:Details
+		});
+}) ;
+
+
+routes.get('/gallery', async (req, res) => {
+	const Details= await Detail.findOne({"id":"64517f235a5fdfd776d5cca7"})
+
+	res.render("gallery",{
+		Details:Details
+	})
+})
+
+module.exports=routes
